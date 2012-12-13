3df42a77da454f54df82571607d954d8afea3707
Anthony Williams <hi@antw.me>
2012-12-13 16:47:45 +0000

    Remove an unnecessary "take" optimisation.

--- lib/refinery/strategies/share/solo.rb
+++ lib/refinery/strategies/share/solo.rb
@@ -4,7 +4,7 @@ module Refinery::Strategies
     # outbound edge.
     class Solo
       def self.calculable?(edge)
-        edge.from.out_edges.take(2).one?
+        edge.from.out_edges.one?
       end
 
       def self.calculate(edge)
