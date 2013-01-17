a451401d8d73ba2881f602bb01b46649ba71f2ae
Anthony Williams <hi@antw.me>
2013-01-17 16:38:02 +0000

    Remove calculators not used by edge demands.

--- spec/refinery/calculators/edge_share_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-module Refinery::Calculators ; describe EdgeShare do
-  # Tests for the edge share calculations are included in the integration
-  # tests directory.
-end ; end # Refinery::Calculators
