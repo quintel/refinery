7b4dcc801cb936027e6edc2aa0b992f1f823f517
Anthony Williams <hi@antw.me>
2013-09-13 14:47:06 +0100

    Remove old ETSource stub

--- lib/refinery.rb
+++ lib/refinery.rb
@@ -44,21 +44,4 @@ require 'refinery/node'
 require 'refinery/reactor'
 require 'refinery/slot'
 require 'refinery/slots_collection'
-require 'refinery/stub'
 require 'refinery/util'
-
-module Refinery
-  module_function
-
-  # Public: Loads the source files into a Turbine graph.
-  #
-  # Presently this creates a stub graph as documented in etsource#168, but in
-  # the future will load CSVs output by InputExcel.
-  #
-  # path - Path to the source files on disk. Currently unused.
-  #
-  # Returns a Turbine::Graph.
-  def load(path = nil)
-    Stub.etsource_168
-  end
-end
--- spec/refinery_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Refinery do
-  context '#load' do
-    it 'returns a Turbine Graph' do
-      expect(Refinery.load).to be_a(Turbine::Graph)
-    end
-  end
-end
