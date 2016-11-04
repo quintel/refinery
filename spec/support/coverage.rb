5c52a2220044662e62daf9a0c08658d732fb1be1
Anthony Williams <hi@antw.io>
2016-11-04 09:44:33 +0000

    Ignore debug tools in test coverage
    These tools typically output PNG diagrams which are hard to test
    properly.

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -3,5 +3,11 @@ if ENV['COVERAGE']
 
   SimpleCov.start do
     add_filter('/spec/')
+
+    # Tools whose output is PNG diagrams to be read by humans; hard to test
+    # these fully.
+    add_filter('/lib/refinery/diagram')
+    add_filter('/lib/refinery/graph_debugger')
+    add_filter('/lib/refinery/catalyst/visual_calculator')
   end
 end
